950edaaee4121b50d3cb8ddfbc4e639c9e30b544
fj <menspocky322@gmail.com>
2022-07-16 10:18:28 +0900

    make convert seconds to timer function

---
+++ src/utils/timer.ts
@@ -0,0 +1,10 @@
+export const secondsToHms = (totalSeconds: number) => {
+  const hour = Math.floor(totalSeconds / 3600)
+  const minute = Math.floor((totalSeconds % 3600) / 60)
+  const seconds = Math.floor((totalSeconds % 3600) % 60)
+
+  const hourDisplay = hour > 0 ? String(hour).padStart(2, '0') : '00'
+  const minuteDisplay = minute > 0 ? String(minute).padStart(2, '0') : '00'
+  const secondsDisplay = seconds > 0 ? String(seconds).padStart(2, '0') : '00'
+  return hourDisplay + minuteDisplay + secondsDisplay
+}
