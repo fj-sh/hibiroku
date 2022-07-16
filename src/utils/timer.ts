2788a85d9091a3b2c304703453a0865502559bab
fj <menspocky322@gmail.com>
2022-07-16 12:07:06 +0900

    Setup ts-jest

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -6,5 +6,5 @@ export const secondsToHms = (totalSeconds: number) => {
   const hourDisplay = hour > 0 ? String(hour).padStart(2, '0') : '00'
   const minuteDisplay = minute > 0 ? String(minute).padStart(2, '0') : '00'
   const secondsDisplay = seconds > 0 ? String(seconds).padStart(2, '0') : '00'
-  return hourDisplay + minuteDisplay + secondsDisplay
+  return `${hourDisplay}:${minuteDisplay}:${secondsDisplay}`
 }
